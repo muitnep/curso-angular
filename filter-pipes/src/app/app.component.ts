8b9c3c0015521883e4bb58fbdb3fd28fd358ea89
Rodrigo Santos Bonifácio Rodrigues <muitnep@gmail.com>
2021-06-08 10:47:20 -0300

    Estudo de filter e pipe

---
+++ filter-pipes/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  hoje = new Date(1980, 0, 24);
+
+  //preco = 59.99;
+
+}
